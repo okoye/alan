1f60459e95c2a672be93769e871fd61303e96ba2
Chuka Okoye <okoye9@gmail.com>
2012-01-17 18:41:24 -0800

    added trainer.js

---
+++ Alan/Resources/lib/trainer.js
@@ -0,0 +1,6 @@
+/**
+ * @author Chuka Okoye
+ * 
+ * Listens for events from processor, fetches appropriate data sets and pushes to api
+ * only when connected to Wifi otherwise just logs last timestamp and waits for Wifi.
+ */
