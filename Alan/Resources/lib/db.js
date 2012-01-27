7d51b541326ce7801aa1b7519c6b5b3934a835a9
Chuka Okoye <okoye9@gmail.com>
2012-01-26 20:01:07 -0800

    updated tiapp.xml to use titanium 1.8 and added jasmine test runners

--- Alan/Resources/lib/db.js
+++ Alan/Resources/lib/db.js
@@ -10,9 +10,21 @@ var local = {};
 var DATABASE_NAME  = 'Alan.db';
 
 local.createTable = function(name){
-	
+	log.info('Creating a new table, if none exists');
+	var db = Ti.Database.open(DATABASE_NAME);
+	db.execute('CREATE TABLE IF NOT EXISTS ? (id INTEGER PRIMARY KEY, timestamp INTEGER NOT NULL, json TEXT NOT NULL)', name);
+	db.close();
 };
 local.deleteTable = function(name){
-	
+	log.info('Dropping table from Alan database');
+	var db = Ti.Database.open(DATABASE_NAME);
+	db.execute('DROP TABLE ?', name);
+	db.close();
 };
-local.insert = function()
+local.insert = function(timestamp, json, table){
+	log.info('Inserting new data into table '+table);
+	var db = Ti.Database.open(DATABASE_NAME);
+	db.execute('INSERT INTO ? (timestamp, json) VALUES (?, ?)', table, timestamp, JSON.stringify(json));
+	db.close();
+};
+
