d85c87fc69807f15424a563c325e4e4229706e7d
Chuka Okoye <okoye9@gmail.com>
2012-01-27 17:12:07 -0800

    bug fixes in db.js, added more tests in test_database

--- Alan/Resources/lib/db.js
+++ Alan/Resources/lib/db.js
@@ -8,6 +8,7 @@ var log = require('lib/logger');
 
 var local = {};
 var DATABASE_NAME  = 'Alan.db';
+var HARD_LIMIT = 100000;
 
 exports.createTable = function(name){
 	log.info('Creating a new table, if none exists');
@@ -23,11 +24,52 @@ exports.deleteTable = function(name){
 	db.close();
 	return true;
 };
-exports.insert = function(timestamp, json, table){
+exports.insert = function(timestamp, json, table){ //Treated like a key, value store.
 	log.info('Inserting new data into table '+table);
 	var db = Ti.Database.open(DATABASE_NAME);
 	var statement = "INSERT INTO "+table+" (timestamp, json) VALUES (?, ?);";
 	db.execute(statement, timestamp, JSON.stringify(json));
 	db.close();
 	return true;
+};
+exports.fetchAll = function(table){
+    log.info('Fetching all data in table '+table);
+    var db = Ti.Database.open(DATABASE_NAME);
+    var statement = "SELECT * FROM "+table;
+    var rows = db.execute(statement);
+    var result = [];
+    var counter = 0;
+    while (rows.isValidRow() && counter < HARD_LIMIT){
+        result.push({
+            id: rows.fieldByName('id'),
+            timestamp: rows.fieldByName('timestamp'),
+            json: rows.fieldByName('json')
+        });
+        counter += 1;
+        rows.next();
+    }
+    rows.close();
+    db.close();
+    return result;
+};
+exports.fetchId = function(table, id){
+  log.info('Fetching datum by specific ID in table '+table);
+  var db = Ti.Database.open(DATABASE_NAME);
+  statement = "SELECT * FROM "+table+" WHERE id=?";
+  var rows = db.execute(statement, id);
+  var counter = 0;
+  var result = [];
+  while (rows.isValidRow() && counter < HARD_LIMIT){
+      result.push({
+          id: rows.fieldByName('id'),
+          timestamp: rows.fieldByName('timestamp'),
+          json: rows.fieldByName('json')
+      });
+      counter += 1;
+      rows.next();
+  }
+  rows.close();
+  db.close();
+  return result;
+  return result;  
 };
--- Alan/Resources/test/tests/test_database.js
+++ Alan/Resources/test/tests/test_database.js
@@ -2,6 +2,10 @@ var db = require('lib/db');
 
 exports.run = function(){
     describe('Database tests', function(){
+       beforeEach(function(){
+          db.createTable('test_database');
+       });
+       
        it('should do nothing.', function(){
           expect(null).toBeNull(); 
        }); 
@@ -16,6 +20,25 @@ exports.run = function(){
        
        it('should drop the table', function(){
            expect(db.deleteTable('test_database')).toBeTruthy();
+       });
+       
+       it('should fetch get result object', function(){
+          expect(db.fetchAll('test_database')).toBeDefined(); 
+       });
+       
+       it('should have 0 and 1 elements', function(){
+          expect(db.fetchAll('test_database').length).toEqual(0);
+          db.insert(0, {test: 'object'}, 'test_database');
+          expect(db.fetchAll('test_database').length).toEqual(1);
+       });
+       
+       it('should have get exact id', function(){
+          db.insert(0, {test: 'object'}, 'test_database');
+          db.insert(0, {test: 'object'}, 'test_database');
+          db.insert(0, {test: 'object'}, 'test_database');
+         expect(db.fetchId('test_database', 1).length).toEqual(1); 
+         var result = db.fetchId('test_database', 1);
+         expect(result[0].id).toEqual(1);
        });
     });
     
