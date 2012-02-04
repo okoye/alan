a056b1b7dd5f604ee08057841147eefcb0fbaf46
Chuka Okoye <okoye9@gmail.com>
2012-02-04 02:01:46 -0800

    added new resource images

--- Alan/Resources/ui/alanWindow.js
+++ Alan/Resources/ui/alanWindow.js
@@ -5,7 +5,10 @@
 
 var styling = require('lib/styles');
 var myAnalytics = reqiure('lib/youView');
-var platformWidth = Ti.Platform.displayCaps.platformWidth;
+
+//Constants
+var PLATFORM_WIDTH = Ti.Platform.displayCaps.platformWidth;
+var PLATFORM_HEIGHT = Ti.Platform.DisplayCaps.platformHeight;
 
 exports.createAlanWindow = function(_args){
     var win = Ti.UI.createWindow({
@@ -13,17 +16,41 @@ exports.createAlanWindow = function(_args){
         orientationModes: [Ti.UI.PORTRAIT],
     }),
     headerView = Ti.UI.createView({
-        backgroundColor: 'pink',
+        backgroundColor: 'pink', //TODO Navigation bar customization and sub header elements
+        height: PLATFORM_HEIGHT - (styling.tabHeight + styling.bodyHeight),
     }),
-    bodyView = myAnalytics.createView(),
-    tabHeight = styling.tabHeight,
     footerView = Ti.UI.createView({
         bottom: 0,
-        height: tabHeight,
+        height: styling.tabHeight,
         backgroundImage: 'images/bottom_nav_bg_alan.png',
-        width: platformWidth
+        width: PLATFORM_WIDTH
     }),
     tabs = [];
     
+    var bodyView = myAnalytics.createView();
+    
+    var createTab = function(_icon, _cb, _on){
+        //TODO
+    };
+    var changeTab = function(tabNo){
+        //TODO
+    };
+    
+    //Create main application tabs
+    tabs.push(createTab('images/bottom_nav_btn_me', function(){
+        changeTab(0);
+    }, true));
+    tabs.push(createTab('images/bottom_nav_btn_life_meter'), function(){
+        changeTab(1);
+    });
+    tabs.push(createTab('images/bottom_nav_btn_rewards'), function(){
+        changeTab(2);
+    });
+    tabs.push(createTab('images/bottom_nav_btn_settings'), function(){
+        changeTab(3);
+    });
+    
+    win.add(headerView);
 };
+
 
