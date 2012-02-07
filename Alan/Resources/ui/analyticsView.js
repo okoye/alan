59a09f02a63575e3f1c8006cf5c331fa7cb53ed7
Chuka Okoye <okoye9@gmail.com>
2012-02-06 17:52:54 -0800

    now working on body base

---
+++ Alan/Resources/ui/analyticsView.js
@@ -0,0 +1,12 @@
+/**
+ * @author Chuka Okoye
+ * 
+ * The You analytics view
+ */
+
+exports.createView = function(_args){
+    //root view control
+    var view = Ti.UI.createView({
+        
+    })
+};
