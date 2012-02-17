53880e882a00478ad6f9386bc6580c071afe765b
Chuka Okoye <okoye9@gmail.com>
2012-02-17 01:13:51 -0800

    added filter module and cache module

---
+++ Alan/Resources/lib/cache.js
@@ -0,0 +1,39 @@
+/**
+ * @author Chuka Okoye
+ * A simple cache abstracting the details of caching.
+ */
+
+var cacheStore = {}; //TODO Use persistent in app store
+
+exports.create = function(name){
+    //create a new cache given the name 'name'
+    cacheStore[name] = [];
+};
+
+exports.add = function(name, data){
+    //push into 'name', this value
+    if (_exists(name)){
+        cacheStore[name].push(data);
+        return true;
+    }
+    else{
+        return false;
+    }
+};
+
+exports.fetch = function(name){
+    //fetch all data for 'name'
+    if (_exists(name)){
+        return cacheStore[name];
+    }
+    else{
+        return null;
+    }
+};
+
+//Some helper functions
+var _exists = function(name){
+    return (cacheStore[name]);
+};
+
+
