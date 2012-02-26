715c51eba184bb10e32f7d28c3d3ff50d0546750
Chuka Okoye <okoye9@gmail.com>
2012-02-26 14:30:41 -0800

    added addon buttons

--- Alan/Resources/ui/alanWindow.js
+++ Alan/Resources/ui/alanWindow.js
@@ -24,12 +24,7 @@ var NUM_TABS = 4;
 exports.createAlanWindow = function(_args){
     var win = Ti.UI.createWindow({
         tabBarHidden: true,
-    }),
-    headerView = Ti.UI.createView({
-        backgroundImage : 'images/top_nav.png',
-        height : 45,
-        top : 0,
-        width: PLATFORM_WIDTH,
+        barImage: 'images/top_nav.png'
     }),
     tabHeight = 50,
     footerView = Ti.UI.createView({
@@ -79,7 +74,6 @@ exports.createAlanWindow = function(_args){
         for (var i=0, l = tabs.length; i < l; i++){
             if (tabNo === i){
                 if(!tabs[i].on){                    
-                    headerView.fireEvent('alan:changeTitle', {no: i});
                     tabs[i].toggle();
                 }
             }
@@ -87,52 +81,31 @@ exports.createAlanWindow = function(_args){
                 tabs[i].toggle();
             }
         }
-    };
-    
-    //Add callbacks, text and other elements to headerview.
-    var updateHeader = function(){
-        headerView.titles = {
-            0: 'Alan',
-            1: 'Activity Age',
-            2: 'Me vs Others',
-            3: 'Store',
-        };
+    };    
+    var headerUpdate = function(_leftNav, _rightNav, _title){
+        (_title) ? dummyTab.title = _title : dummyTab.title = "";
         
-        var titleLabel = Ti.UI.createLabel({
-            text: headerView.titles[0],
-            color: '#fff',
-            textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
-            height: 'auto',
-            width: 'auto',
-            font: {fontFamily: 'Arial', fontSize: 20, fontWeight: 'bold'},
-            shadowColor: '#4e869c',
-            shadowOffset: {x:-1, y:-1}
-        });
-        
-        headerView.add(titleLabel);
-        //TODO: switch from listener to callback function.
-        headerView.addEventListener('alan:changeTitle', function(e){
-            titleLabel.text = headerView.titles[e.no];
-        });
-        return headerView;
     };
-    
     //Create main application tabs
     tabs.push(createTab('images/bottom_nav_btn_me', function(){
         changeTab(0);
         bodyView.updateStrip(0);
+        headerUpdate(0);
     }, true));
     tabs.push(createTab('images/bottom_nav_btn_act_age', function(){
         changeTab(1);
         bodyView.updateStrip(1);
+        headerUpdate(1);
     }));
     tabs.push(createTab('images/bottom_nav_btn_connect', function(){
         changeTab(2);
         bodyView.updateStrip(2);
+        headerUpdate(2);
     }));
-    tabs.push(createTab('images/bottom_nav_btn_store', function(){
+    tabs.push(createTab('images/bottom_nav_btn_addon', function(){
         changeTab(3);
         bodyView.updateStrip(3);
+        headerUpdate(3);
     }));
     
     //Tabs to footer view
