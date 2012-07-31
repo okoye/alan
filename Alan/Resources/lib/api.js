4fab08fe2f405c35eaaa1d8c2e5edd7f3ef8c7c5
Chuka Okoye <okoye9@gmail.com>
2012-07-31 04:50:54 -0700

    reverted to non array based success code. also, api endpoints have context interpretation of specific error codes

--- Alan/Resources/lib/api.js
+++ Alan/Resources/lib/api.js
@@ -20,8 +20,8 @@ var connector = function(callback, success, username, password){
     var conn = Ti.Network.createHTTPClient({
         onload: function(e){
             var status = this.status;
-            if (success.indexOf(status) != -1){
-                log.debug('Successful HTTP code from API');
+            if (status == success){
+                log.debug('Successful HTTP code from API')
                 data = this.responseText;
                 data = (data) ? data:"{}";
                 callback({status: 'success', data: JSON.parse(data)});
@@ -30,24 +30,16 @@ var connector = function(callback, success, username, password){
                 log.debug('Bad HTTP code from API')
                 callback({
                     status: 'error',
-                    message: 'Cannot not connect to Alan network at this time.'
+                    code: this.status,
                 });
             }
         },
         onerror: function(e){
-        	if (success.indexOf(status) != -1){
-        		log.debug('Successful HTTP code from API');
-        		data = this.responseText;
-        		data = (data) ? data: "{}";
-        		callback({status: 'success', data:JSON.parse(data)});
-        	}
-        	else{
-        		log.debug('An error occured when contacting API '+JSON.stringify(e));
-            	callback({
-                	status: 'error',
-                	message: 'Could not connect to Alan. Try again later.'
-            	});
-        	}
+            log.debug('An error occured when contacting API '+JSON.stringify(e));
+            callback({
+                status: 'error',
+                code: this.status,
+            });
         },
         timeout: 5000,
         enableKeepAlive: false
@@ -65,7 +57,18 @@ var connector = function(callback, success, username, password){
 
 exports.CreateAccount = function(account, profile, callback){
 	log.info('Sending email & password to api');
-	var conn = connector(callback, [204, 409]);
+	var evaluate = function(response){
+		if (response.status === 'error'){
+			if (response.code == 409){
+				response.message = 'An account with same username already exists';
+			}
+			else{
+				response.message = 'Hmmm...you may be having network issues. Cannot connect to Alan.';
+			}
+			callback(reponse);
+		}
+	};
+	var conn = connector(evaluate, 204);
 	conn.open('POST', base+'/1/accounts/basic');
 	var packet = {};
 	for (prop in account){
@@ -80,7 +83,21 @@ exports.CreateAccount = function(account, profile, callback){
 
 exports.UpdateSensor = function(account, readings, callback){
     log.info('Updating sensors on api '+JSON.stringify(readings));
-    var conn = connector(callback, [204], account.get('username'), account.get('password'));
+    var evaluate = function(response){
+    	if (response.status === 'error'){
+    		if (response.code == 401){
+    			response.message = 'Cannot login to your Alan account.';
+    		}
+    		else if(response.status == 500){
+    			response.message = 'Oops, looks like Alan network is still being upgraded.';
+    		}
+    		else{
+    			response.message = 'Your wireless network connection is somewhat spotty.';
+    		}
+    		callback(response);
+    	}
+    };
+    var conn = connector(evaluate, 204, account.get('username'), account.get('password'));
     conn.open('POST', base+'/1/sensors/update');
     conn.send(JSON.stringify(readings));
     testflight.checkpoint('api.updatesensor');
@@ -88,7 +105,21 @@ exports.UpdateSensor = function(account, readings, callback){
 
 exports.Analytics = function(account, callback){
     log.info('Retrieving Analytics data');
-    var conn = connector(callback, [200], account.get('username'), account.get('password'));
+    var evaluate = function(response){
+    	if (response.status === 'error'){
+    		if (response.code == 401){
+    			response.message = 'Cannot login to your Alan account.';
+    		}
+    		else if(response.status == 500){
+    			response.message = 'Alan network is currently experiencing issues.';
+    		}
+    		else{
+    			response.message = 'Looks like you have poor internet connectivity at this time.'
+    		}
+    		callback(response);
+    	}
+    }
+    var conn = connector(evaluate, 200, account.get('username'), account.get('password'));
     conn.open('GET', base+'/1/analytics');
     conn.send();
     testflight.checkpoint('api.analytics');
