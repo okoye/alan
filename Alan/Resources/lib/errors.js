4e974aebfaebcfa8d406125063faf6fed12786fb
Chuka Okoye <okoye9@gmail.com>
2012-07-31 15:16:19 -0700

    added error definitions

---
+++ Alan/Resources/lib/errors.js
@@ -0,0 +1,11 @@
+/**
+ * @author Chuka Okoye
+ * Describes common errors thrown by our app.
+ */
+
+exports.codes = {
+	AuthenticationError: 0,
+	InternalError: 1,
+	NetworkError: 2,
+	DuplicateAccount: 3,
+};
