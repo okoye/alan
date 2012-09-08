28b87ee8c5dd1ead19a3d530c931335c3a26912b
Chuka Okoye <okoye9@gmail.com>
2012-09-08 11:26:13 -0700

    fo

---
+++ Counting/Counting/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Counting
+//
+//  Created by Chuka Okoye on 9/8/12.
+//  Copyright (c) 2012 Chuka Okoye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Counting/CountingTests/CountingTests.m
@@ -0,0 +1,32 @@
+//
+//  CountingTests.m
+//  CountingTests
+//
+//  Created by Chuka Okoye on 9/8/12.
+//  Copyright (c) 2012 Chuka Okoye. All rights reserved.
+//
+
+#import "CountingTests.h"
+
+@implementation CountingTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CountingTests");
+}
+
+@end
