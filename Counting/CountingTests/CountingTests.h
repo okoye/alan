708c3ff64d9bf6fd92cfb9c669d709a382347535
Chuka Okoye <okoye9@gmail.com>
2012-09-08 11:30:49 -0700

    added new counting project

---
+++ Counting/CountingTests/CountingTests.h
@@ -0,0 +1,13 @@
+//
+//  CountingTests.h
+//  CountingTests
+//
+//  Created by Chuka Okoye on 9/8/12.
+//  Copyright (c) 2012 Chuka Okoye. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CountingTests : SenTestCase
+
+@end
