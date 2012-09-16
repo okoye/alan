375ce3019e0253e475e5ee6639a41a9c23c7f7b0
Chuka Okoye <okoye9@gmail.com>
2012-09-15 21:47:25 -0700

    added aggro project

---
+++ Aggro/Aggro/AggroViewController.m
@@ -0,0 +1,34 @@
+//
+//  AggroViewController.m
+//  Aggro
+//
+//  Created by Chuka Okoye on 9/15/12.
+//  Copyright (c) 2012 Lightcurves Labs. All rights reserved.
+//
+
+#import "AggroViewController.h"
+
+@interface AggroViewController ()
+
+@end
+
+@implementation AggroViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
